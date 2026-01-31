8cdfd6a0f8471d357495f92835f314898565c2dc
julianasilvanascimento995-dotcom <julianasilva.nascimento995@gmail.com>
2026-01-31 14:42:54 -0300

    Add files via upload

---
+++ calculadoraprof.py
@@ -0,0 +1,13 @@
+print('Calculadora Python')
+
+n1 = float(input('= '))
+n2 = float(input('= '))      
+
+# f_string
+
+print(f'''
+Soma - {n1 +  n2}
+Substração -  {n1  - n2}
+Multiplicação -  {n1 * n2}
+Divisão  -  {n1 / n2}
+''')
