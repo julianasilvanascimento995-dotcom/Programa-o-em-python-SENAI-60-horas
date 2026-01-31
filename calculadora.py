3fcd27711b4f12483c681c095e56e6ef950cb076
julianasilvanascimento995-dotcom <julianasilva.nascimento995@gmail.com>
2026-01-31 14:35:09 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,21 @@
+print('CALCULADORA PYTHON')
+print("_" *100)
+print()
+
+n1 = float( input('digite um numero:'))
+n2= float( input('digite um numero:'))
+print()
+
+soma = n1 + n2
+sub = n1 - n2
+div = n1 / n2
+mult = n1 * n2
+resto = n1 % n2
+pot = n1 ** n2
+
+print("o resultado a soma é: ", soma)
+print("o resultado a subtracao é:", sub)
+print("o resultado a divisão é: ", div)
+print("o resultado da multiplicação é: ", mult)
+print("o resultado do resto é: ", resto)
+print("o resultado da potencia é: ", pot)
